d55386e7c9956a35fea5cbc9c25f6ad72c55ff86
adrianpalomares <adrianpal45@gmail.com>
2020-03-14 23:45:24 -0700

    added movie description

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const superagent = require("superagent");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
 
 // Setting up ability to use environment file
 require("dotenv").config();
 
-const _print = results => {
+const _printResults = results => {
     results.forEach((result, index) => {
         console.log(`${index}) ${result.Title}`);
     });
+};
+
+const _printMovieDesc = result => {
+    console.log();
+    console.log(figlet.textSync(result.Title));
+    console.log();
+    console.log(`Year: ${result.Year}`);
+    console.log(`Rating: ${result.Rated}`);
+    console.log(`Actors: ${result.Actors}`);
+    console.log(`Plot: ${result.Plot}`);
+    console.log();
+};
+
+const _choose = () => {
+    return inquirer.prompt([
+        {
+            type: "number",
+            name: "choice",
+            message: "What movie would you like to choose?"
+        }
+    ]);
 };
 
 // Main function
@@ -20,7 +43,17 @@ async function search(query) {
     const searchResults = searchResponse.body.Search;
 
     // Print results
-    _print(searchResults);
+    _printResults(searchResults);
+
+    // Choosing a result through a prompt
+    const movieChoice = await _choose();
+
+    // The choice will be in movieChoice.choice <- A number(index)
+    const movieInfoResponse = await superagent.get(
+        baseUrl + `&i=${searchResults[movieChoice.choice].imdbID}`
+    );
+
+    _printMovieDesc(movieInfoResponse.body);
 }
 
 module.exports = {
